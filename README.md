640fd37f3d2c0a721d64e2e984e6d3ddd596cf03
Massimiliano Galli <massimiliano.galli@cern.ch>
2025-09-17 16:24:00 +0200

    cms configs files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SBI $H \rightarrow \tau \tau$ EFT
+# SBI $H \rightarrow \tau \tau$ EFT samples production
 
 ## Setup
 
@@ -12,25 +12,156 @@ To setup the environment and the necessary software stack (on lxplus) run:
 This part describes the instructions to generate gridpacks, LHE, nanogen and
 nanoaod files for a certain process. We will consider as an example ```ppH_Htt_SMEFTsim_topU3l_quadratic```, but it will be the same for all of them.
 
+### MG cards and EFT (TODO)
+
 ### Creating gridpack
 
-The creation of gridpacks follows the standard procedure explained
-[here](https://cms-generators.docs.cern.ch/how-to-produce-gridpacks/mg5-amcnlo/). The MG cards are thus stored inside ```madgraph-cards```, divided per process, with the naming scheme required by the genproduction scripts.
+The creation of gridpacks follows the standard procedure explained [here](https://cms-generators.docs.cern.ch/how-to-produce-gridpacks/mg5-amcnlo/). The MG cards are thus stored inside ```madgraph-cards```, divided per process, with the naming scheme required by the genproduction scripts.
 First, access the correct path:
 ```
 cd genproductions_scripts/bin/MadGraph5_aMCatNLO
 ```
 
-As exaplined in the documentation, gridpacks for Run 3 should be produced using
-```cmssw-el8```. Following the instructions
-[here](https://cms-sw.github.io/singularity.html), on lxplus we just activate it
-by typing ```cmssw-el8```.
+As exaplined in the documentation, gridpacks for Run 3 should be produced using ```cmssw-el8```. Following the instructions [here](https://cms-sw.github.io/singularity.html), on lxplus we just activate it by typing ```cmssw-el8```.
 To generate the gridpack we then run:
 ```
 ./gridpack_generation.sh ppH_Htt_SMEFTsim_topU3l_quadratic ../../../madgraph-cards/ppH_Htt_SMEFTsim_topU3l_quadratic
 ```
-This will produce a gridpack called
-```ppH_Htt_SMEFTsim_topU3l_quadratic_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz```.
-Note that the procedure runs locally; in order to run this procedure on
-HTCondor, check out the documentation for
-```submit_condor_gridpack_generation.sh```.
+This will produce a gridpack called ```ppH_Htt_SMEFTsim_topU3l_quadratic_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz```.
+Note that the procedure runs locally; in order to run this procedure on HTCondor, check out the documentation for ```submit_condor_gridpack_generation.sh```.
+
+### Creating EDM GEN files
+
+This part will require to exit from ```cmssw-el8```. First of all, change the path inside the pythia fragment at ```CMSSW_13_2_9/src/Configuration/GenProduction/python/pythia_fragment.py``` to the one that was just created. 
+We can then create the config file by running (taken from the [cmseft repo](https://github.com/FNALLPC/cmseft/tree/main)):
+```
+cmsDriver.py Configuration/GenProduction/python/pythia_fragment.py \
+    --mc \
+    --python_filename ppH_Htt_SMEFTsim_topU3l_quadratic_gen_cfg.py \
+    --eventcontent RAWSIM,LHE \
+    --datatier GEN,LHE \
+    --conditions 130X_mcRun3_2023_realistic_postBPix_v5 \
+    --beamspot Realistic25ns13p6TeVEarly2023Collision \
+    --step LHE,GEN \
+    --nThreads 1 \
+    --geometry DB:Extended \
+    --era Run3_2023 \
+    --customise Configuration/DataProcessing/Utils.addMonitoring \
+    --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=123 \
+    --fileout file:gen.root \
+    --no_exec -n 100
+```
+and run it with:
+```
+cmsRun ppH_Htt_SMEFTsim_topU3l_quadratic_gen_cfg.py
+```
+
+### Creating NanoGEN files
+
+The NanoGEN format can be used for exploratory studies (not for the full analysis setup, as in this case we need reco-level quantities). A NanoGEN config file can be produced with:
+```
+cmsDriver.py Configuration/GenProduction/python/pythia_fragment.py \
+    --python_filename ppH_Htt_SMEFTsim_topU3l_quadratic_nanogen_cfg.py --eventcontent NANOAODGEN \
+    --customise Configuration/DataProcessing/Utils.addMonitoring --datatier NANOAOD \
+    --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=123 \
+    --fileout file:nanogen.root --conditions 130X_mcRun3_2023_realistic_postBPix_v5 --beamspot Realistic25ns13p6TeVEarly2023Collision \
+    --step LHE,GEN,NANOGEN --geometry DB:Extended --era Run3_2023 --no_exec --mc -n 100
+```
+
+The different weights, taken from the ```reweight_card.dat```, can be saved by adding lines like
+```
+named_weights = [
+    "rw0000",
+    "rw0001",
+]
+process.genWeightsTable.namedWeightIDs = named_weights
+process.genWeightsTable.namedWeightLabels = named_weights
+```
+(TODO script to aumomate will be written).
+
+### Creating NanoAOD files
+
+In this part we summarize the procedure to produce nanoAOD files for a sample (i.e. with reconstruction included). The procedure can be find in different twikis, for convenience we base ours on [this](https://indico.cern.ch/event/1500035/contributions/6575125/attachments/3091743/5476084/IITH_GEN_Tutorial.pdf).
+Note that, in order for the commands to work and have tags and CMSSW versions that are compatible with each other, the best idea is to take a NanoAOD sample similar to what we want to generate and copy the config commands.
+This example is made using [this workflow](https://cms-pdmv-prod.web.cern.ch/mcm/chained_requests?prepid=HIG-chain_Run3Summer23BPixwmLHEGS_flowRun3Summer23BPixDRPremix_flowRun3Summer23BPixMiniAODv4_flowRun3Summer23BPixNanoAODv13-00001&page=0&shown=15), but could be different for other processes.
+
+SIM step:
+```
+cmsDriver.py \
+    --python_filename ppH_Htt_SMEFTsim_topU3l_quadratic_sim_cfg.py \
+    --eventcontent RAWSIM \
+    --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM \
+    --fileout file:sim.root --conditions 130X_mcRun3_2023_realistic_postBPix_v5 --beamspot Realistic25ns13p6TeVEarly2023Collision \
+    --step SIM --geometry DB:Extended --filein file:gen.root --era Run3_2023 --no_exec --mc -n 100
+```
+```
+cmsRun ppH_Htt_SMEFTsim_topU3l_quadratic_sim_cfg.py
+```
+
+DIGI-RAW step (TODO resources for careful and meaningful inclusion of pileup and HLT have to be found):
+```
+cmsDriver.py \
+    --python_filename ppH_Htt_SMEFTsim_topU3l_quadratic_digi_cfg.py \
+    --eventcontent PREMIXRAW \
+    --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW \
+    --fileout file:digi.root \
+    --pileup_input "dbs:/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer23BPix_130X_mcRun3_2023_realistic_postBPix_v1-v1/PREMIX" \
+    --conditions 130X_mcRun3_2023_realistic_postBPix_v5 \
+    --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:2023v12 \
+    --procModifiers premix_stage2,siPixelQualityRawToDigi --geometry DB:Extended --filein file:sim.root \
+    --datamix PreMix --era Run3_2023 --no_exec --mc -n 100
+```
+```
+cmsRun ppH_Htt_SMEFTsim_topU3l_quadrati_digi_cfg.py
+```
+
+RECO/AOD step:
+```
+cmsDriver.py \
+    --python_filename ppH_Htt_SMEFTsim_topU3l_quadratic_reco_cfg.py \
+    --eventcontent AODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM \
+    --fileout file:aod.root --conditions 130X_mcRun3_2023_realistic_postBPix_v5 --step RAW2DIGI,L1Reco,RECO,RECOSIM \
+    --geometry DB:Extended --filein file:digi.root --era Run3_2023 --no_exec --mc -n 100
+```
+```
+cmsRun ppH_Htt_SMEFTsim_topU3l_quadratic_reco_cfg.py
+```
+
+MINIAOD step:
+```
+cmsDriver.py \
+    --python_filename ppH_Htt_SMEFTsim_topU3l_quadratic_miniaod_cfg.py --eventcontent MINIAODSIM \
+    --customise Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM \
+    --fileout file:miniaod.root --conditions 130X_mcRun3_2023_realistic_postBPix_v5 \
+    --step PAT --geometry DB:Extended --filein file:aod.root --era Run3_2023 \
+    --no_exec --mc -n 100
+```
+```
+cmsRun ppH_Htt_SMEFTsim_topU3l_quadratic_miniaod_cfg.py
+```
+
+NANOAOD step:
+```
+cmsDriver.py  \
+    --eventcontent NANOAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier NANOAODSIM \
+    --conditions 133X_mcRun3_2023_realistic_postBPix_ForNanov13_v2 --step NANO \
+    --scenario pp --era Run3_2023,run3_nanoAOD_124 --python_filename ppH_Htt_SMEFTsim_topU3l_quadratic_nanoaod_cfg.py \
+    --fileout file:nanoaod.root --filein file:miniaod.root --no_exec --mc -n 100
+```
+Also in this case, the different weights, taken from the ```reweight_card.dat```, can be saved by adding lines like
+```
+named_weights = [
+    "rw0000",
+    "rw0001",
+]
+process.genWeightsTable.namedWeightIDs = named_weights
+process.genWeightsTable.namedWeightLabels = named_weights
+```
+Finally, run:
+```
+cmsRun ppH_Htt_SMEFTsim_topU3l_quadratic_nanoaod_cfg.py
+```
+
+### Scale out (TODO)
+
+Here we show how to use CRAB to produce NanoAOD with the relevant EFT weights. Relevant documentation can be found [here](https://twiki.cern.ch/twiki/bin/view/CMSPublic/CRAB3AdvancedTopic)
---
+++ nanoaod_cfgs/ppH_Htt_SMEFTsim_topU3l_quadratic_nanogen_cfg.py
@@ -0,0 +1,219 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/pythia_fragment.py --python_filename ppH_Htt_SMEFTsim_topU3l_quadratic_nanogen_cfg.py --eventcontent NANOAODGEN --customise Configuration/DataProcessing/Utils.addMonitoring --datatier NANOAOD --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=123 --fileout file:nanogen.root --conditions 130X_mcRun3_2023_realistic_postBPix_v5 --beamspot Realistic25ns13p6TeVEarly2023Collision --step LHE,GEN,NANOGEN --geometry DB:Extended --era Run3_2023 --no_exec --mc -n 10
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run3_2023_cff import Run3_2023
+
+process = cms.Process('NANOGEN',Run3_2023)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic25ns13p6TeVEarly2023Collision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('PhysicsTools.NanoAOD.nanogen_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    accelerators = cms.untracked.vstring('*'),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
+    dumpOptions = cms.untracked.bool(False),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+            allowAnyLabel_=cms.required.untracked.uint32
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(0)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    holdsReferencesToDeleteEarly = cms.untracked.VPSet(),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    modulesToIgnoreForDeleteEarly = cms.untracked.vstring(),
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(0),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Configuration/GenProduction/python/pythia_fragment.py nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.NANOAODGENoutput = cms.OutputModule("NanoAODOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(9),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('NANOAOD'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:nanogen.root'),
+    outputCommands = process.NANOAODGENEventContent.outputCommands
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '130X_mcRun3_2023_realistic_postBPix_v5', '')
+
+process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CP5Settings',
+            'processParameters'
+        ),
+        processParameters = cms.vstring(
+            'JetMatching:setMad = off',
+            'JetMatching:scheme = 1',
+            'JetMatching:merge = on',
+            'JetMatching:jetAlgorithm = 2',
+            'JetMatching:etaJetMax = 5.',
+            'JetMatching:coneRadius = 1.',
+            'JetMatching:slowJetPower = 1',
+            'JetMatching:qCut = 30.',
+            'JetMatching:nQmatch = 5',
+            'JetMatching:nJetMax = 1',
+            'JetMatching:doShowerKt = off'
+        ),
+        pythia8CP5Settings = cms.vstring(
+            'Tune:pp 14',
+            'Tune:ee 7',
+            'MultipartonInteractions:ecmPow=0.03344',
+            'MultipartonInteractions:bProfile=2',
+            'MultipartonInteractions:pT0Ref=1.41',
+            'MultipartonInteractions:coreRadius=0.7634',
+            'MultipartonInteractions:coreFraction=0.63',
+            'ColourReconnection:range=5.176',
+            'SigmaTotal:zeroAXB=off',
+            'SpaceShower:alphaSorder=2',
+            'SpaceShower:alphaSvalue=0.118',
+            'SigmaProcess:alphaSvalue=0.118',
+            'SigmaProcess:alphaSorder=2',
+            'MultipartonInteractions:alphaSvalue=0.118',
+            'MultipartonInteractions:alphaSorder=2',
+            'TimeShower:alphaSorder=2',
+            'TimeShower:alphaSvalue=0.118',
+            'SigmaTotal:mode = 0',
+            'SigmaTotal:sigmaEl = 21.89',
+            'SigmaTotal:sigmaTot = 100.309',
+            'PDF:pSet=LHAPDF6:NNPDF31_nnlo_as_0118'
+        ),
+        pythia8CommonSettings = cms.vstring(
+            'Tune:preferLHAPDF = 2',
+            'Main:timesAllowErrors = 10000',
+            'Check:epTolErr = 0.01',
+            'Beams:setProductionScalesFromLHEF = off',
+            'SLHA:minMassSM = 1000.',
+            'ParticleDecays:limitTau0 = on',
+            'ParticleDecays:tau0Max = 10',
+            'ParticleDecays:allowPhotonRadiation = on'
+        )
+    ),
+    comEnergy = cms.double(13600.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(1)
+)
+
+
+process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/afs/cern.ch/work/g/gallim/Postdoc/Generation/SBI-Htt-EFT/genproductions_scripts/bin/MadGraph5_aMCatNLO/ppH_Htt_SMEFTsim_topU3l_quadratic_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz'),
+    nEvents = cms.untracked.uint32(10),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+
+# Path and EndPath definitions
+process.lhe_step = cms.Path(process.externalLHEProducer)
+process.generation_step = cms.Path(process.pgen)
+process.nanoAOD_step = cms.Path(process.nanogenSequence)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.NANOAODGENoutput_step = cms.EndPath(process.NANOAODGENoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.lhe_step,process.generation_step,process.genfiltersummary_step,process.nanoAOD_step,process.endjob_step,process.NANOAODGENoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+#Setup FWK for multithreaded
+process.options.numberOfConcurrentLuminosityBlocks = 1
+process.options.eventSetup.numberOfConcurrentIOVs = 1
+# filter all path with the production filter sequence
+for path in process.paths:
+	if path in ['lhe_step']: continue
+	getattr(process,path).insert(0, process.generator)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from PhysicsTools.NanoAOD.nanogen_cff
+from PhysicsTools.NanoAOD.nanogen_cff import customizeNanoGEN 
+
+#call to customisation function customizeNanoGEN imported from PhysicsTools.NanoAOD.nanogen_cff
+process = customizeNanoGEN(process)
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
+
+
+# Customisation from command line
+
+process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=123
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
+
+named_weights = [
+    "rw0000",
+    "rw0001",
+]
+process.genWeightsTable.namedWeightIDs = named_weights
+process.genWeightsTable.namedWeightLabels = named_weights
